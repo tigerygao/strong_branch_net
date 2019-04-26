b9b381df7a634d328b2b6bd0cf0d190a22f11baf
thomaskeady <you@example.com>
2019-04-26 09:39:38 +0000

    automator works for 1x, going to naively replicate a bunch with some variations

--- automator.py
+++ automator.py
@@ -11,7 +11,7 @@ num_features = 6; # How modify the set of features from here?
 hidden_layers = [30, 50, 50, 10];
 epochs = 3;
 
-print("%s, %d, %d, %s, %d" % dataset, strong_branching_limit, num_features, str(hidden_layers), epochs);
+print("%s, %d, %d, %s, %d" % (dataset, strong_branching_limit, num_features, str(hidden_layers), epochs));
 admipex1(dataset, strong_branching_limit, num_features, hidden_layers, epochs);
 
 print ("end");
