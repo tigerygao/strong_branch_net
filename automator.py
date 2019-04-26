068ea10a01dd0f6591f7733c20ae31608e3a0d48
Thomas Keady <thomas.keady@jhu.edu>
2019-04-26 16:59:54 -0400

    implementatoin in progress

--- automator.py
+++ automator.py
@@ -1,19 +1,78 @@
 
 from admipex1 import admipex1
 import time
+from datetime import datetime
+
+# just for preallocating size of arrays, if you run more tests than this just increase this number
+max = 1000;
+
+# List of things we can change in test runs
+dataset                         = [];
+strong_branching_limit          = [];
+num_features                    = [];
+hidden_layers                   = []; # for now must be 4 layers
+epochs                          = [];
+#num_random_seeds                = []; # Hold off for now
+
+# Holds output filenames
+outputs = [];
 
 
-dataset                 = [];
-strong_branching_limit  = [];
-num_features            = [];
-hidden_layers           = [];
-epochs                  = [];
-num_random_seeds        = [];
+# If you want to add another test run, put it here
+
+run = 0;
+dataset[run]                      = "air04.mps.gz";
+strong_branching_limit[run]       = 10000000; # Set very high to be 100% (full?) strong branching
+num_features[run]                 = 7; # How modify the set of features from here?
+hidden_layers[run]                = [10, 20, 20, 10];
+epochs[run]                       = 5;
+outputs[run] = datetime.today().strftime('%Y-%M-%D_%H-%M-%S_') \
+                    + dataset[run] + "_" \
+                    + str(strong_branching_limit[run]) + "_" \
+                    + num_features[run] + "_" \
+                    + "[" + ','.join(str(l) for l in hidden_layers[run]) + "]_" \
+                    + str(epochs[run]) \
+                    + ".csv";
+
+
+run = run + 1;
+dataset[run]                      = "air04.mps.gz";
+strong_branching_limit[run]       = 45; # Set very high to be 100% (full?) strong branching
+num_features[run]                 = 7; # How modify the set of features from here?
+hidden_layers[run]                = [10, 20, 20, 10];
+epochs[run]                       = 5;
+
+run = run + 1;
+dataset[run]                      = "air04.mps.gz";
+strong_branching_limit[run]       = 45; # Set very high to be 100% (full?) strong branching
+num_features[run]                 = 7; # How modify the set of features from here?
+hidden_layers[run]                = [10, 20, 20, 10];
+epochs[run]                       = 5;
+
+run = run + 1;
+dataset[run]                      = "air04.mps.gz";
+strong_branching_limit[run]       = 45; # Set very high to be 100% (full?) strong branching
+num_features[run]                 = 7; # How modify the set of features from here?
+hidden_layers[run]                = [10, 20, 20, 10];
+epochs[run]                       = 5;
+
+run = run + 1;
+dataset[run]                      = "air04.mps.gz";
+strong_branching_limit[run]       = 45; # Set very high to be 100% (full?) strong branching
+num_features[run]                 = 7; # How modify the set of features from here?
+hidden_layers[run]                = [10, 20, 20, 10];
+epochs[run]                       = 5;
+
+run = run + 1;
+dataset[run]                      = "air04.mps.gz";
+strong_branching_limit[run]       = 45; # Set very high to be 100% (full?) strong branching
+num_features[run]                 = 7; # How modify the set of features from here?
+hidden_layers[run]                = [10, 20, 20, 10];
+epochs[run]                       = 5;
 
 
 
-# If you 
-
+print("Doing %d test runs" % run);
 
 ''' # Just strong branching by setting sbl very high
 print ("start");
