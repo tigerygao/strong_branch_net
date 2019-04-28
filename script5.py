7d1df15f85c555dc794c21fb7121ae32f8e3f9ab
Thomas Keady <thomas.keady@jhu.edu>
2019-04-28 03:28:22 -0400

    AUTOMATED COMMIT: 2019-04-28_03-28-06_neos-1109824.mps.gz_10000000_7_[10,20,20,10]_5.csv

--- script5.py
+++ script5.py
@@ -39,7 +39,7 @@ gpu = 0
 # Dataset if you want all of them to be same (None if you dont);
 #ds = None;
 #ds = "air04.mps.gz"; # For testing purposes, (make sure to set sbl to <12)
-ds = "n4-3.mps.gz";
+ds = "neos-1109824.mps.gz";
 
 # SBL if you want all of them to be same (None if you dont);
 #sbl = None;
